e79577ff5a87b58d5c721483aedb9df070dc0853
YousefAlaa44 <67795824+YousefAlaa44@users.noreply.github.com>
2023-08-29 14:50:42 +0200

    Player Controller

---
+++ FPS-Shooting/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float Speed = 5f;
+
+    PlayerController controller;
+    Camera maincamera;
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        maincamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 MovInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 playerVelocity = MovInput.normalized * Speed;
+
+        controller.Move(playerVelocity);
+
+        Ray ray = maincamera.ScreenPointToRay(Input.mousePosition);
+
+        Plane GroundPlane = new Plane(Vector3.up, Vector3.zero);
+        float RayDistence;
+        if (GroundPlane.Raycast(ray,out RayDistence))
+        {
+            Vector3 Point = ray.GetPoint(RayDistence);
+            //Debug.DrawLine(ray.origin, Point, Color.red);
+            controller.LookAt(Point);
+        }
+
+        
+    }
+}
---
+++ FPS-Shooting/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody Rg;
+    Vector3 Velocity;
+    void Start()
+    {
+        Rg = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _Velocity)
+    {
+        Velocity = _Velocity;   
+    }
+
+    public void LookAt(Vector3 LookPoint)
+    {
+        Vector3 CorrectHeight = new Vector3(LookPoint.x, transform.position.y, LookPoint.z);
+        transform.LookAt(CorrectHeight);
+    }
+
+    void FixedUpdate()
+    {
+        Rg.MovePosition(Rg.position + Velocity * Time.fixedDeltaTime);
+    }
+}
